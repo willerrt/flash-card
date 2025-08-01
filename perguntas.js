537494de8862a68d782c78f2fdb2275afce14e2d
willerrt <thalyson.barbosa@escola.pr.gov.br>
2025-08-01 09:41:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criarCartao(
+    "Categoria",    
+    "Pergunta",  
+    "Resposta" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+criarCartao(
+    "Dinossauro",    
+    "Qual o mlr dinossauro",  
+    "todos" 
+);
+
+document.querySelectorAll('#container .cartao').forEach(cartao => {
+    if (!cartao.innerHTML.trim()) {
+        cartao.remove();
+    }
+});
